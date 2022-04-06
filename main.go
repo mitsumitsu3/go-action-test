224f5b7e3f03fbf9a9d8d5f3c934301b8344b0c7
mitsu <d.mitsuoka@newbees.jp>
2022-04-06 17:41:44 +0900

    text change commit

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "hello test")
+		return c.String(http.StatusOK, "hello")
 	})
 
 	e.Logger.Fatal(e.Start(":8080"))
